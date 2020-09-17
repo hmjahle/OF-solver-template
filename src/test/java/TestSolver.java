ff19390d16b6e7a2ecd00f710e4e69eea9445208
Carl Andreas Julsvoll <carl.julsvoll@visma.com>
2020-09-17 14:32:16 +0200

    template updated with or-tools

---
+++ src/test/java/TestSolver.java
@@ -0,0 +1,13 @@
+import com.visma.opt.KaSolver;
+import org.junit.Test;
+
+public class TestSolver {
+
+
+    @Test
+    public void testSolver() {
+        KaSolver kaSolver = new KaSolver();
+        kaSolver.solve();
+    }
+
+}
