37fa263a74a770a79b63109d4dc313178191db2e
Carl Andreas Julsvoll <carl.julsvoll@visma.com>
2020-09-18 10:11:22 +0200

    added comment

--- src/main/java/com/visma/opt/SolverTemplate.java
+++ src/main/java/com/visma/opt/SolverTemplate.java
@@ -13,6 +13,7 @@ public class SolverTemplate extends Solver {
 
     static {
         SolverProvider.registerSolver(new SolverTemplate());
+        System.out.println("printing java library path");
         System.out.println(System.getProperty("java.library.path"));
         System.loadLibrary("jniortools");
     }
